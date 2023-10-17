05766f41bb87b87d7deb20d592906af2760a9c9b
Ahmad Naufal <91902493+Fallid@users.noreply.github.com>
2023-10-17 15:26:47 +0700

    Delete Modul 1 directory
    tidak terpakai lagi

--- Modul 1/prak_modul_1/lib/app/Routes/name_page.dart
+++
@@ -1,6 +0,0 @@
-class PageName {
-  // Navigation Menu
-  static const notification = "/notification";
-  static const home = "/home";
-  static const profile = "/profile";
-}
--- Modul 1/prak_modul_1/lib/app/modules/Profile/bindings/profile_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:prak_modul_1/app/modules/Profile/controllers/profile_controller.dart';
-
-class ProfileBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<ProfileController>(() => ProfileController());
-  }
-}
