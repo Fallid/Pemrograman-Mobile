86545d1b00a5f72c8319a3376bd972d64f41938b
Izza Ihsan Fathony <izzafathony27@gmail.com>
2023-11-29 09:40:53 +0700

    push database controller

---
+++ Modul5/modul_5_latihan/lib/app/modules/home/controllers/database_controller.dart
@@ -0,0 +1,43 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:get/get.dart';
+import 'package:modul_5_latihan/app/modules/home/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+
+class DatabaseController extends HomeController {
+  Databases? databases;
+  @override
+  void onInit() {
+    super.onInit();
+    // appwrite
+    databases = Databases(client);
+  }
+
+  Future storeUserName(Map map) async {
+    try {
+      final result = await databases!.createDocument(
+        databaseId: "6566a2a7c8864b45af9e",
+        documentId: ID.unique(),
+        collectionId: "6566a2c22a98ceeca0b2",
+        data: map,
+        permissions: [
+          Permission.read(Role.user("6566a454a09037206388")),
+          Permission.update(Role.user("6566a454a09037206388")),
+          Permission.delete(Role.user("6566a454a09037206388")),
+        ],
+      );
+      print("DatabaseController:: storeUserName $databases");
+    } catch (error) {
+      Get.defaultDialog(
+        title: "Error Database",
+        titlePadding: const EdgeInsets.only(top: 15, bottom: 5),
+        titleStyle: Get.context?.theme.textTheme.titleLarge,
+        content: Text(
+          "$error",
+          style: Get.context?.theme.textTheme.bodyMedium,
+          textAlign: TextAlign.center,
+        ),
+        contentPadding: const EdgeInsets.only(top: 5, left: 15, right: 15),
+      );
+    }
+  }
+}
