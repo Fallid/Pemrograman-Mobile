9be5d267e3dffd62481c68cd84e992c379df42af
arizkyiqbal <arizkyiqbal@gmail.com>
2023-11-29 09:10:15 +0700

    push app write

--- Modul5/modul_5_latihan/lib/app/modules/home/controllers/home_controller.dart
+++ Modul5/modul_5_latihan/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,38 @@
 import 'package:get/get.dart';
+import 'package:appwrite/appwrite.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
+
+  // @override
+  // void onReady() {
+  //   super.onReady();
+  // }
+
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
+
+  // void increment() => count.value++;
+
+  Client client = Client();
+
   @override
   void onInit() {
     super.onInit();
+    // appwrite
+    const endPoint = "https://cloud.appwrite.io/v1'";
+    const projectID = "65661953844d0b4181d0";
+    client
+        .setEndpoint(endPoint)
+        .setProject(projectID)
+        .setSelfSigned(status: true);
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
