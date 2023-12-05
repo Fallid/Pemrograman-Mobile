1442d8bca13f08ffe3cc05ba8070b288dcd79535
Fallid <naufalopal002@gmail.com>
2023-12-05 23:19:26 +0700

    feat: Endpoint and user client appwrite

--- Modul5/modul_5_prak/lib/app/data/api/appwrite.dart
+++ Modul5/modul_5_prak/lib/app/data/api/appwrite.dart
@@ -1,6 +1,7 @@
 class AppWrite {
   static const Endpoint = "https://cloud.appwrite.io/v1";
-  static const CollectionId = "";
-  static const DatabaseId = "";
+  static const CollectionId = "656f4d2e4cba7fee5d28";
+  static const DatabaseId = "656f4d0a69a1f16c389f";
   static const ProjectId = "65661953844d0b4181d0";
+  static const BucketId = "6566a4d59e526e57587c";
 }
